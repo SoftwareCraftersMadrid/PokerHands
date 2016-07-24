ec3a27e82ab404610d7786e91f46f212caa34157
Juan David Vega <jdvr.93@hotmail.com>
2016-07-24 10:24:39 +0200

    green: twoPairs should return true when the hand contains 2 different pairs

--- src/card.js
+++ src/card.js
@@ -1,9 +1,12 @@
+var weightOrder = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 function Card(value, suit) {
     var self = this;
     self.value = value;
     self.suit = suit;
+    self.weigh = weightOrder[self.value];
     return {
         value: self.value,
+        weigh: self.weigh,
         justValueEqual: function (otherCard) {
           return self.value == otherCard.value
         },
--- src/rules/twoPairs.js
+++ src/rules/twoPairs.js
@@ -1,5 +1,18 @@
+
+var isRepeated = (element, list) => list.filter((item) => element.value === item.value).length == 2;
+
+var hasNotCheckedBefore =
+    (element, checkedElements) => checkedElements.filter(
+        (checkedElement) => checkedElement.value === element.value
+    ).length == 0;
+
 function check(hand) {
-    return false;
+    var checkedElements = [];
+    hand.forEach(function (element, index, elements) {
+        if(hasNotCheckedBefore(element, checkedElements) && isRepeated(element, elements))
+            checkedElements.push(element);
+    });
+    return checkedElements.length == 2;
 }
 
 module.exports.check = check;
