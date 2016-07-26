15102a5f9eedff1b851ecd33e706727a53bef51e
Juan David Vega <jdvr.93@hotmail.com>
2016-07-26 19:48:26 +0200

    green: straight flush

--- src/rules/straightFlush.js
+++ src/rules/straightFlush.js
@@ -1,7 +1,8 @@
+var hasAStraight = require('./straight').check;
+var hasAFlush = require('./flush').check;
 
 function check(hand) {
-
+    return hasAFlush(hand) && hasAStraight(hand);
 }
-
 
 module.exports.check = check;
