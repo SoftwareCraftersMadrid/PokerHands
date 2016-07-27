fd730e72eb71bbc6ddf3f9befc8aced72f2fe2d3
Juan David Vega <jdvr.93@hotmail.com>
2016-07-27 19:57:21 +0200

    move apply logic to checker

--- src/pockerHandTypeChecker.js
+++ src/pockerHandTypeChecker.js
@@ -1,7 +1,21 @@
 var rules = require('./rules');
 
+var descending = (leftRule, rightRule) => rules.indexOf(rightRule) - rules.indexOf(leftRule);
+
+var rulesThatAppliedTo = (hand) => (rule) => rule.check(hand);
+
+
+function applyTo(hand) {
+
+    return rules.filter(
+        rulesThatAppliedTo(hand)
+    ).sort(
+        descending
+    )[0].type;
+}
+
 function check(hand) {
-    return rules.applyTo(hand);
+    return applyTo(hand);
 }
 
 module.exports.check = check;
--- src/rules/index.js
+++ src/rules/index.js
@@ -20,18 +20,5 @@ var rules = [
     straightFlush
 ] ;
 
-var descending = (leftRule, rightRule) => rules.indexOf(rightRule) - rules.indexOf(leftRule);
 
-var rulesThatAppliedTo = (hand) => (rule) => rule.check(hand);
-
-
-function applyTo(hand) {
-
-    return rules.filter(
-        rulesThatAppliedTo(hand)
-    ).sort(
-        descending
-    )[0].type;
-}
-
-module.exports.applyTo = applyTo;
+module.exports = rules;
